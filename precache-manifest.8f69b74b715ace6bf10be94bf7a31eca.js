b997a0b812e313e0e320a40dcea1e4e469ef40cc
brandongatlin <brandongatlin1981@me.com>
2022-04-04 20:43:39 -0500

    Updates

---
+++ precache-manifest.8f69b74b715ace6bf10be94bf7a31eca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1801bb9744acca264c6a9d8b0b8decc7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fb124dc5b387e89c8a62",
+    "url": "/portfolio/static/css/main.df7bb556.chunk.css"
+  },
+  {
+    "revision": "2d3b657a450e443967e2",
+    "url": "/portfolio/static/js/2.0785a73d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.0785a73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb124dc5b387e89c8a62",
+    "url": "/portfolio/static/js/main.76fcfa69.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
